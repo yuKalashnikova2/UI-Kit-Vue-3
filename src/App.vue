b6e4dc2aae33bdc21c7beb3bad84c185a9e08a7e
yuKalasnikova2 <pmalian@list.ru>
2023-07-01 14:44:51 +0700

    feat(components): Button

--- src/App.vue
+++ src/App.vue
@@ -47,7 +47,7 @@ const toggleOpen = () => {
   position: fixed;
   left: 0;
   width: 15px;
-  background-color: var(--primary-color);
+  background-color: var(--primary);
   height: 100%;
   top: 62px;
   z-index: 1;
--- src/views/Button.vue
+++ src/views/Button.vue
@@ -1,3 +1,49 @@
+<script setup>
+import Button from '../components/Button.vue'
+</script>
 <template>
-    <h1>Button</h1>
+  <h1 class="heading-1">Button</h1>
+  <h2>Disabled</h2>
+  <div class="line">
+    <Button label="Primary" color="primary" disabled />
+    <Button label="Second" color="second" disabled />
+  </div>
+
+  <h2>Colors</h2>
+  <div class="line">
+    <Button label="Primary" color="primary" />
+    <Button label="Second" color="second" />
+    <Button label="Success" color="success" />
+    <Button label="Info" color="info" />
+    <Button label="Warning" color="warning" />
+    <Button label="Danger" color="danger" />
+  </div>
+
+  <h2>Rounded</h2>
+  <div class="line">
+    <Button label="Primary" color="primary" rounded />
+    <Button label="Second" color="second" rounded />
+    <Button label="Success" color="success" rounded />
+    <Button label="Info" color="info" rounded />
+    <Button label="Warning" color="warning" rounded />
+    <Button label="Danger" color="danger" rounded />
+  </div>
+
+  <h2>Outlined</h2>
+  <div class="line">
+    <Button label="Primary" color="primary" outlined />
+    <Button label="Second" color="second" outlined />
+    <Button label="Success" color="success" outlined />
+    <Button label="Info" color="info" outlined />
+    <Button label="Warning" color="warning" outlined />
+    <Button label="Danger" color="danger" outlined />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.line {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+}
+</style>
