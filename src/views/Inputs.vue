7a819eb9f6b0acff937444df4ca016983e331859
yuKalasnikova2 <pmalian@list.ru>
2023-07-11 19:46:31 +0700

    Inputs: password

--- src/views/Inputs.vue
+++ src/views/Inputs.vue
@@ -1,12 +1,16 @@
 <script setup>
 import { ref, computed } from 'vue'
 import useValidate from '@vuelidate/core'
-import { helpers, minLength, email } from '@vuelidate/validators'
+import { helpers, minLength, email, sameAs } from '@vuelidate/validators'
 
 import Inputs from '../components/Inputs.vue'
+import Button from '../components/Button.vue'
+
 
 const nameField = ref('')
 const emailField = ref('')
+const passwordField = ref('')
+const confirmPasswordField = ref('')
 
 const rules = computed(() => ({
   nameField: {
@@ -15,16 +19,29 @@ const rules = computed(() => ({
   emailField: {
     email: helpers.withMessage(`Вы ввели неверный email`, email),
   },
+  confirmPasswordField: {
+    sameAsPassword: helpers.withMessage(
+      `Пароли не совпадают`,
+      sameAs(passwordField.value)
+    ),
+  },
 }))
 
-const v = useValidate(rules, { nameField, emailField })
+const v = useValidate(rules, { nameField, emailField, confirmPasswordField })
+
+const submitForm = () => {
+    v.value.$touch()
+    if(v.value.$error) return
+    alert('Form submitted')
+}
 </script>
 
 <template>
   <h1>Inputs</h1>
   <h2>Hi there! I'm input</h2>
-  <form>
+  <form @submit.prevent="submitForm">
     <Inputs
+      type="text"
       placeholder="Enter your name"
       name="name"
       label="Your name"
@@ -32,11 +49,30 @@ const v = useValidate(rules, { nameField, emailField })
       :error="v.nameField.$errors"
     />
     <Inputs
+      type="email"
       placeholder="Enter your email"
       name="email"
       label="Your email"
       v-model:value="v.emailField.$model"
       :error="v.emailField.$errors"
     />
+    <Inputs
+      type="password"
+      placeholder="Enter your password"
+      name="password"
+      label="Your password"
+      v-model:value="passwordField"
+      :error="v.emailField.$errors"
+    />
+    <Inputs
+      type="password"
+      placeholder="Confirm your password"
+      name="confirmPasswordField"
+      label="Confirm your password"
+      v-model:value="v.confirmPasswordField.$model"
+      :error="v.confirmPasswordField.$errors"
+    />
+    <Button label="Submit" color="info" />
+
   </form>
 </template>
